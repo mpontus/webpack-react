8afa79f9540c8a4a04a8c7b9167ea9bd806c8a6d
Michael Pontus <m.pontus@gmail.com>
2019-09-18 13:52:27 +0300

    Minimal webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = function(_env, argv) {
+  const isProduction = argv.mode === "production";
+  const isDevelopment = !isProduction;
+
+  return {
+    devtool: isDevelopment && "cheap-module-source-map",
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "assets/js/[name].[contenthash:8].js",
+      publicPath: "/"
+    }
+  };
+};
